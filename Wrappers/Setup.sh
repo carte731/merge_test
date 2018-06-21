e6ef5c12cdeab847348db11dc873fb078c09d5f4
carte731 <carte731@umn.edu>
2018-06-20 21:41:18 -0500

    some xz stuff

--- Wrappers/Setup.sh
+++ Wrappers/Setup.sh
@@ -60,11 +60,11 @@ case "${setup_routine}" in
 #	cp ~/xz-5.2.4.tar.gz xz-5.2.4.tar.gz
 #	tar -xvf xz-5.2.4.tar.gz
 	git clone https://github.com/xz-mirror/xz.git
-	git reset --hard 9815cdf6987ef91a85493bfcfd1ce2aaf3b47a0a
+#	git reset --hard 9815cdf6987ef91a85493bfcfd1ce2aaf3b47a0a
 #	rm xz-5.2.4.tar.gz
 #	cd xz-5.2.4
 	cd xz
-#	git reset --hard 9815cdf6987ef91a85493bfcfd1ce2aaf3b47a0a
+	git reset --hard 9815cdf6987ef91a85493bfcfd1ce2aaf3b47a0a
 	./autogen.sh
 	./configure --prefix=$(pwd -P)
 	make
