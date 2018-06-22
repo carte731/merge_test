99767df5fac5e821d7bd920363cfb520b9458dfa
carte731 <carte731@umn.edu>
2018-06-21 23:29:25 -0500

    making adjustments.

--- Wrappers/Setup.sh
+++ Wrappers/Setup.sh
@@ -94,7 +94,8 @@ case "${setup_routine}" in
 	make
 #        make prefix=`pwd` install
 	make install
-	export LD_LIBRARY_PATH=${xzPath}/lib
+#	export LD_LIBRARY_PATH=${xzPath}/lib
+	echo "export LD_LIBRARY_PATH=${xzPath}/lib:"'${LD_LIBRARY_PATH}' >> ~/.bash_profile
         HTSLIB_DIR=`pwd`
         cd "${ROOT}"
         #   Install ANGSD
